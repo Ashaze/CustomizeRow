33cf91daef6aa768afa85c00a7811eaf40d9ffb1
AshineXu <key1045@163.com>
2017-09-08 15:30:04 +0800

    Ashine
    upload

---
+++ src/js/customizerow.js
@@ -0,0 +1,61 @@
+;(function(root,factory){
+	root.customizeRow = factory()
+})(window,function(){
+	var customizeRow = function(options){
+		this.opt = {};
+		this.extend(options);
+		this.init();
+	};
+	customizeRow.prototype={
+		extend:function(options){
+			var defaults = {
+				ellipsis : "cus-row-ellipsis",
+				rowclass : ".cus-row",
+				brows:false
+			}
+			var key;
+			for(key in options){
+				var defaultValue = defaults[key];
+				var optionValue = options[key];
+				if(optionValue == defaultValue ){ continue}
+				else if(optionValue !== undefined){
+					defaults[key] = optionValue;
+				}
+			}
+			this.opt = defaults;
+		},
+		init:function(){
+			var that = this.opt;
+			var userAgent = navigator.userAgent;
+			if((userAgent.indexOf("Chrome")>-1)&&that.brower){return}
+
+			
+			$(that.rowclass).each(function(){
+				var _this = $(this);
+				_this.height('auto');
+				var text = _this.text(),
+					width = _this.width(),
+					textIndent = parseInt(_this.css("text-indent")),
+					letterSpacing = _this.css("letter-spacing"),
+					MAXROW = _this.attr("cus-row"),
+					LINEHEIGHT = _this.css("line-height"),
+					FONTSIZE = parseInt(_this.css("font-size")),
+					contentHeight = _this.height();
+				if(LINEHEIGHT == "normal"){
+					LINEHEIGHT = Math.ceil(FONTSIZE * 1.14);
+				}
+				var rowHeight = parseInt(LINEHEIGHT) * MAXROW;
+				if(contentHeight > rowHeight){
+					_this.addClass(that.ellipsis);
+					var a = FONTSIZE + letterSpacing;
+					var b = width * MAXROW - textIndent;
+					_this.attr("title",text);
+					_this.text(text.substr(0,(parseInt(b/a) - MAXROW)));
+				} 
+				_this.height(rowHeight + 2*MAXROW);
+			})
+		}
+	}
+	return customizeRow;
+});
+
